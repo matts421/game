be7af8aa490530112c023ce369193771d4b590d3
pmacau <pmacau@Macap.>
2025-09-05 14:49:33 -0700

    inbounded the ball

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use bevy::prelude::*;
 
+pub const WINDOW_WIDTH: i32 = 1366;
+pub const WINDOW_HEIGHT: i32 = 768;
+
 fn main() {
-    App::new().run();
+    App::new() 
+        .add_plugins(DefaultPlugins)
+        .run();
     println!("Program finished running.");
 }
