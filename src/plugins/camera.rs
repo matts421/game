b0914b81e670ce13088271d941d98209982a772c
pmacau <pmacau@students.cs.ubc.ca>
2025-09-07 12:23:32 -0700

    added camera movement on the player

--- src/plugins/camera.rs
+++ src/plugins/camera.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::plugins::player::Player;
+
 const CAMERA_DISTANCE: f32 = 80.0; 
 
 pub struct CameraPlugin; 
@@ -7,7 +9,8 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin{
     fn build(&self, app: &mut App){
         app
-            .add_systems(Startup, spawn_camera);
+            .add_systems(Startup, spawn_camera)
+            .add_systems(Update, camera_follow);
     }
 }
 
@@ -19,5 +22,11 @@ fn spawn_camera(mut commands: Commands){
     ));
 }
 
+fn camera_follow(mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<Player>)>, player_query: Query<&Transform, With<Player>>){
+    let player_translation = player_query.single().unwrap().translation; // might have to change when multiplayer
+    let mut camera_transform = camera_query.single_mut().unwrap();
+    camera_transform.translation = player_translation + Vec3::new(0.0, CAMERA_DISTANCE, CAMERA_DISTANCE); 
+    
+}
 
 
