9fd8a556726833386a78e62da9d83c8a71189b5f
Matthew <matthewasmith442@gmail.com>
2025-09-08 21:06:48 -0700

    add workflow for builds

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 use bevy::prelude::*;
 use bevy_panorbit_camera::PanOrbitCameraPlugin;
 
-pub mod plugins; 
+pub mod plugins;
 
-use plugins::scene::ScenePlugin; 
+use plugins::camera::CameraPlugin;
+use plugins::ground_sample::GroundPlugin;
 use plugins::movement::MovementPlugin;
 use plugins::player::PlayerPlugin;
 use plugins::print::PrintPlugin;
-use plugins::camera::CameraPlugin; 
-use plugins::ground_sample::GroundPlugin;
+use plugins::scene::ScenePlugin;
 
 pub const WINDOW_WIDTH: i32 = 1366;
 pub const WINDOW_HEIGHT: i32 = 768;
 
-
-
 fn main() {
-    App::new() 
+    App::new()
         .add_plugins(ScenePlugin)
         .add_plugins(PanOrbitCameraPlugin)
         .add_plugins(PlayerPlugin)
         .add_plugins(MovementPlugin)
-        .add_plugins(CameraPlugin) 
+        .add_plugins(CameraPlugin)
         .add_plugins(PrintPlugin)
         .add_plugins(GroundPlugin)
         .add_plugins(DefaultPlugins)
         .run();
     println!("Program finished running.");
 }
-
